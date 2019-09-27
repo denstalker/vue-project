343fa1a68480bad4dc1eba9d73c283fa538ab4b2
Denis <brenkov_d@inbox.ru>
2019-09-27 17:55:41 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <b-alert show>Hello {{ name }}!</b-alert>
+    <b-spinner label="Spinning"></b-spinner>
+    <b-spinner type="grow" label="Spinning"></b-spinner>
+    <b-spinner variant="primary" label="Spinning"></b-spinner>
+    <b-spinner variant="primary" type="grow" label="Spinning"></b-spinner>
+    <b-spinner variant="success" label="Spinning"></b-spinner>
+    <b-spinner variant="success" type="grow" label="Spinning"></b-spinner>
+    <!-- <app-car></app-car> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+// import Car from "./components/Cars.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+    // appCar: Car
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -26,3 +36,4 @@ export default {
   margin-top: 60px;
 }
 </style>
+   
---
+++ src/components/Cars.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>Название : {{carName}}</h3>
+    <p>Год выпуска : {{carYear}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+        carName: 'Ford',
+        carYear: 2008
+    };
+  }
+};
+</script>
